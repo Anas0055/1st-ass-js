e03002d66285c02608be769266e51809c69ce11b
Muhammad Anas <151996955+Anas0055@users.noreply.github.com>
2024-07-27 20:52:54 -0700

    first commit

---
+++ data-types.js
@@ -0,0 +1,16 @@
+                // DATA AND TYPES
+// 1. Declare a variable called 'is Island' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+
+// 2. Log the types of 'is Island', 'population', 
+// 'country' and 'language' to the console
+let isIsland=false;
+let language;
+let country="canada";
+let population="200 M";
+ console.log("type of isIsland:"), typeof isIsland;
+ console.log("type of population:"), typeof population;
+ console.log("type of country:"), typeof country;
+ console.log("type of language:"), typeof language;
+ 
---
+++ index.js
@@ -0,0 +1,12 @@
+            // VALUES AND VARIABLES
+// 1. Declare variables callad 'country','continent' and population'
+// and assign their values according to your own country (populations)
+// 2. log their values to the console.
+
+
+let country="pakistan"
+let continent="asia"
+let population= "22 M"
+console.log(country)
+console.log(continent)
+console.log(population)
